7e6552fcac9d73124789a758c2cf6930dd632086
ximenap1205 <xparedes40@gmail.com>
2024-08-16 12:58:00 -0300

    manejador de eventos

---
+++ eventos.js
@@ -0,0 +1,7 @@
+const divContenedor = document.getElementById('div-contenedor');
+
+function FuncionClick(){
+    alert('Hola! Soy el div');
+}
+
+divContenedor.addEventListener('click',FuncionClick);
